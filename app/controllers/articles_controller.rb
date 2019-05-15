e6f0c87cc339322f864aae2ac12f364d791b0d73
Muminjon <muminjonguru@outlook.com>
2019-05-15 23:00:57 +0500

    first blogger commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+    include ArticlesHelper
+   
+    def index
+        @articles = Article.all            
+    end 
+
+    def show
+        @articles = Article.find(params[:id])
+    end
+
+    def new 
+        @articles = Article.new
+    end
+
+    def create
+        @articles = Article.new(article_params)
+        @articles.save
+        redirect_to article_path(@articles)
+    end
+
+    def destroy
+        @articles = Article.find(params[:id])
+        @articles.destroy
+        flash.notice = "Article '#{@articles.title}' Deleted!"
+        redirect_to article_path
+    end
+
+    def edit
+        @articles = Article.find(params[:id])
+    end
+
+    def update
+        @articles = Article.find(params[:id])
+        @articles.update(article_params)
+
+        flash.notice = "Article '#{@articles.title}' Updated!"
+
+        redirect_to article_path(@articles)
+    end
+
+      
+
+end
