e36bf29e01781469a01b773e4691cde83d1a5ad4
Muminjon <muminjonguru@outlook.com>
2019-05-22 17:38:24 +0500

    minor bugs fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
   root to: 'articles#index'
-  resources :articles do
+  resources :articles do 
     resources :comments
   end
   resources :tags
   resources :authors
   resources :author_sessions, only: [ :new, :create, :destroy ]
-  get 'login'   => 'author_sessions#new'
-  get 'logout'  => 'author_sessions#destroy'
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
